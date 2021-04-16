c7e874b964063b30a07c7e5a14b685ee6e671b44
dmitriy <dmitriyburenok@gmail.com>
2021-04-15 22:30:18 -0700

    logo size

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import UBCGradeBotLogo from '../logo.svg';
 const Header = () => {
     return (
         <>
-            <img width="350px" src={UBCGradeBotLogo} alt="React Logo" />
+            <img width="300px" src={UBCGradeBotLogo} alt="React Logo" />
             {/* <h1>UBC Grade Bot</h1> */}
         </>
     )

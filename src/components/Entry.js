fa0a3bbf23d17e33009a6daf626612a19e4def22
dmitriy <dmitriyburenok@gmail.com>
2021-04-18 19:59:54 -0700

    how to added

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -5,11 +5,13 @@ const Entry = () => {
 
   const [loading, setLoading] = useState(false)
   const [failed, setFailed] = useState(false)
+  const [tutorial, setTutorial] = useState(true)
   const [stats, setStats] = useState('')
   const [text, setText] = useState('')
 
   const onSubmit = (e) => {
     e.preventDefault()
+    setTutorial(false)
     setFailed(false)
     setLoading(true)
     fetchGrades()
@@ -29,6 +31,20 @@ const Entry = () => {
 
   return (
     <>
+      {(() => {
+        if (tutorial) {
+          return (<div className="tutorial">
+            <h2>How do I use this?</h2>
+            <p>To see stats for a year range, enter "<span className="mono">MATH 101 2014-2020</span>" or just "<span className="mono">WRDS 150 2019</span>" for a single year.</p>
+            <br />
+            <p>You can also choose the session with "<span className="mono">2019s</span>" or "<span className="mono">2019w</span>".</p>
+            {/* <p> <span className="mono">math 101 2014-2020</span></p> */}
+            {/* <p>Do you do stuff</p> */}
+          </div>)
+        }
+      })()}
+
+
       <form onSubmit={onSubmit}>
         <input className="mainEntry" type="text" onChange={event => setText(event.target.value)} placeholder="ex. math 101 2014-2020" />
         <button type="submit" hidden>Submit</button>

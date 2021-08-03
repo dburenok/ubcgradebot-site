26bb264abc5705e10e36dc3a6c564485a445b635
Dmitriy Burenok <dmitriyburenok@gmail.com>
2021-08-03 14:45:12 -0700

    icon

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       </div> */}
       <Header />
       <Entry />
-      <div id="watermark">made by <a href="https://dburenok.com/">dmitriy</a> with <span id="icon_offset"><FaHeart color="orange" /></span></div>
+      <div id="watermark">made by <a href="https://dburenok.com/">dmitriy</a></div>
       <div id="bottom_left"><a href="https://github.com/dburenok/ubcgradebot-site">GitHub Repo</a></div>
     </div>
   );

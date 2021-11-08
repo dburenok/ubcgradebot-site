253152fff5690bcc48d70dcf0eafdec529c2678f
Dmitriy Burenok <dmitriyburenok@gmail.com>
2021-11-08 00:41:13 -0800

    added shadow to loading div

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import Header from './components/Header'
 import Entry from './components/Entry'
-import { FaHeart } from 'react-icons/fa';
 
 function App() {
 
   return (
     <div className='container'>
-      {/* <div className="maintenance">
-        <p>Down for maintenance (1-2 hours)</p>
-      </div> */}
       <Header />
       <Entry />
       <div id="watermark">made by <a href="https://dburenok.com/">dmitriy</a></div>
